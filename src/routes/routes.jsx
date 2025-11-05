cbd4245d860a8d0a90b69f00c95309a94e157cfb
man7788 <jerman1841@gmail.com>
2025-11-05 12:18:14 +0800

    add routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,22 @@
 import App from '../App';
 import ErrorPage from '../components/error/ErrorPage';
+import Login from '../components/login/Login';
+import Dashboard from '../components/dashboard/Dashboard';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
     errorElement: <ErrorPage />,
   },
 ];
