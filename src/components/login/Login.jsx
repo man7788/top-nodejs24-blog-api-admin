1ceb763eca1f3fea5afa3aa931abe80073f0cce1
man7788 <jerman1841@gmail.com>
2025-11-05 11:09:42 +0800

    create login component

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,11 @@
+import styles from './Login.module.css';
+
+const Login = () => {
+  return (
+    <div className={styles.Login}>
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default Login;
