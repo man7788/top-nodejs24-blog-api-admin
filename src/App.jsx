c2d2c53e812800eef870076099e21bf71b4118c4
man7788 <jerman1841@gmail.com>
2025-11-05 12:20:23 +0800

    add redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import styles from './App.module.css';
+import { Navigate } from 'react-router';
 
 function App() {
+  const token = localStorage.getItem('token');
+
   return (
     <div className={styles.App}>
       <h1>Dashboard</h1>
+      {token ? (
+        <Navigate to="/dashboard" replace={true} />
+      ) : (
+        <Navigate to="/login" replace={true} />
+      )}
     </div>
   );
 }
