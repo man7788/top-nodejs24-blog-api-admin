f3474e89ca17f88b6d729ab500b67cbf5480438d
man7788 <jerman1841@gmail.com>
2025-11-09 12:28:53 +0800

    update Dashboard component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,23 @@
 import styles from './Dashboard.module.css';
+import { Outlet, Link } from 'react-router';
+import useAuth from '../../hooks/useAuth';
 
 const Dashboard = () => {
+  const { logout } = useAuth();
+
   return (
     <div className={styles.Dashboard}>
       <h1>Dashboard</h1>
+      <aside>
+        <nav>
+          <Link to="/dashboard">Overview</Link>
+          <br></br>
+          <Link to="/dashboard/account">Account</Link>
+          <br></br>
+          <button onClick={logout}>Log Out</button>
+        </nav>
+      </aside>
+      <Outlet />
     </div>
   );
 };
