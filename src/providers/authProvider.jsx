dc019a76046e03dc2336dd9cfe6d03f59e99ffb6
man7788 <jerman1841@gmail.com>
2025-11-09 12:34:26 +0800

    create authContext

---
+++ src/providers/authProvider.jsx
@@ -0,0 +1,63 @@
+import { useNavigate } from 'react-router';
+import { useState, useEffect } from 'react';
+import AuthContext from '../context/authContext';
+import verifyToken from '../api/verifyToken';
+
+function AuthProvider({ children }, props) {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      const result = await verifyToken();
+
+      if (result.error) {
+        setLoading(false);
+        setError(result.error);
+        return;
+      }
+
+      if (result.user) {
+        setLoading(false);
+        setUser(result.user);
+        return;
+      }
+    };
+
+    verifyUser();
+  }, []);
+
+  const login = async () => {
+    const result = await verifyToken();
+
+    if (result.error) {
+      setError(result.error);
+      return result.error;
+    }
+
+    if (result.user) {
+      setUser(result.user);
+      navigate('/dashboard');
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+    navigate('/login', { replace: true });
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, loading, error, login, logout }}
+      {...props}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
