4c8e6347fbd4d411d6f67fbf8f27fe1a8a6b4eb6
man7788 <jerman1841@gmail.com>
2025-11-12 02:31:41 +0800

    create usePosts hook

---
+++ src/hooks/usePosts.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+
+const usePosts = () => {
+  const [posts, setPosts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/posts/admin`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+            mode: 'cors',
+          },
+        );
+
+        if (response.status === 401) {
+          const result = await response.json();
+          const error = new Error(result.error.message);
+          error.statusCode = result.error.code;
+          throw error;
+        }
+
+        if (response.status > 400) {
+          const result = await response.json();
+          throw new Error(result.error.message);
+        }
+
+        const result = await response.json();
+        const data = result.data.posts;
+
+        setPosts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { posts, error, loading };
+};
+
+export default usePosts;
