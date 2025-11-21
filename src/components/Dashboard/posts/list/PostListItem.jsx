15fcfd35dfd4cad5ebcd201c02dc657fbb1251a6
man7788 <jerman1841@gmail.com>
2025-11-21 11:15:46 +0800

    create post list item component

--- src/components/Dashboard/posts/list/PostListItem.jsx
+++ src/components/Dashboard/posts/list/PostListItem.jsx
@@ -1,12 +1,39 @@
 import styles from './PostListItem.module.css';
 import { Link } from 'react-router';
+import submitPostDelete from '../../../../api/submitPostDelete';
 
-const PostListItem = ({ post }) => {
+const PostListItem = ({
+  post,
+  setListError,
+  setListLoading,
+  update,
+  setUpate,
+}) => {
+  const submitDelete = async (e) => {
+    e.preventDefault();
+
+    setListLoading(true);
+
+    const result = submitPostDelete(post.id);
+
+    if (result.error) {
+      setListError(true);
+      setListLoading(false);
+      return;
+    }
+
+    setUpate(!update);
+    setListLoading(false);
+  };
+
   return (
     <div className={styles.PostListItem}>
       <ul>
         <li>{post.title}</li>
         <Link to={`${post.id}/edit`}>Edit</Link>
+        <form onSubmit={submitDelete}>
+          <input type="submit" value="Delete" />
+        </form>
       </ul>
     </div>
   );
