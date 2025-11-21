e40b0aec0d980723d900a1b673a7ceb8c1217045
man7788 <jerman1841@gmail.com>
2025-11-21 11:15:56 +0800

    create post list component

--- src/components/Dashboard/posts/list/PostList.jsx
+++ src/components/Dashboard/posts/list/PostList.jsx
@@ -1,19 +1,23 @@
 import styles from './PostList.module.css';
+import { useState } from 'react';
 import usePosts from '../../../../hooks/usePosts';
 import PostListItem from './PostListItem';
 
 const PostList = () => {
-  const { posts, error, loading } = usePosts();
+  const { posts, error, loading, update, setUpate } = usePosts();
 
-  if (loading) {
+  const [listError, setListError] = useState(null);
+  const [listLoading, setListLoading] = useState(null);
+
+  if (loading || listLoading) {
     return <h1>loading...</h1>;
   }
 
-  if (error?.statusCode === 401) {
+  if (error?.statusCode === 401 || listError?.statusCode === 401) {
     return <h1>401 - Unauthorized</h1>;
   }
 
-  if (error) {
+  if (error || listError) {
     return <h1>A network error was encountered</h1>;
   }
 
@@ -23,7 +27,13 @@ const PostList = () => {
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
-            <PostListItem post={post} />
+            <PostListItem
+              post={post}
+              setListError={setListError}
+              update={update}
+              setUpate={setUpate}
+              setListLoading={setListLoading}
+            />
           </li>
         ))}
       </ul>
