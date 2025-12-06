97212d2a076db6765c87675e571f829ab39960f9
man7788 <jerman1841@gmail.com>
2025-12-07 01:52:14 +0800

    update CommentListItem component

--- src/components/Dashboard/comments/list/CommentListItem.jsx
+++ src/components/Dashboard/comments/list/CommentListItem.jsx
@@ -1,5 +1,6 @@
 import styles from './CommentListItem.module.css';
 import { Link } from 'react-router';
+import dateFormatter from '../../../../utils/dateFormatter';
 import submitCommentDelete from '../../../../api/submitCommentDelete';
 
 const CommentListItem = ({
@@ -9,6 +10,8 @@ const CommentListItem = ({
   update,
   setUpate,
 }) => {
+  const formattedDate = dateFormatter(comment.createdAt);
+
   const submitDelete = async (e) => {
     e.preventDefault();
 
@@ -27,20 +30,29 @@ const CommentListItem = ({
   };
 
   return (
-    <div className={styles.PostListItem}>
-      <ul>
-        <li>{comment.name}</li>
-        <li>{comment.email}</li>
-        <li>{comment.content}</li>
-        <li>{comment.post.title}</li>
-        <Link to={`${comment.id}/edit`} state={{ postId: comment.postId }}>
-          Edit
-        </Link>
-        <form onSubmit={submitDelete}>
-          <input type="submit" value="Delete" />
-        </form>
-      </ul>
-    </div>
+    <li className={styles.CommentListItem}>
+      <div className={styles.author}>
+        <p className={styles.p}>{comment.name}</p>
+        <p className={styles.p}>{comment.email}</p>
+      </div>
+      <div className={styles.comment}>
+        <p className={styles.p}>{comment.content}</p>
+      </div>
+      <div className={styles.post}>
+        <p className={styles.p}>{comment.post.title}</p>
+      </div>
+      <div className={styles.submmited}>{formattedDate}</div>
+      <Link
+        className={styles.link}
+        to={`${comment.id}/edit`}
+        state={{ postId: comment.postId }}
+      >
+        Edit
+      </Link>
+      <form className={styles.form} onSubmit={submitDelete}>
+        <input type="submit" value="Delete" />
+      </form>
+    </li>
   );
 };
 
