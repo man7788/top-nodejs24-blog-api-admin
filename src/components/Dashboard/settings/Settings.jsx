69741e95c4a0d09517e1b7a89e636d36c099c9cd
man7788 <jerman1841@gmail.com>
2025-12-07 01:54:17 +0800

    update Settings component

--- src/components/Dashboard/settings/Settings.jsx
+++ src/components/Dashboard/settings/Settings.jsx
@@ -1,14 +1,46 @@
 import styles from './Settings.module.css';
-import { Outlet, Link } from 'react-router';
+import { useState, useEffect } from 'react';
+import { useLocation, Outlet, Link } from 'react-router';
 
 const Settings = () => {
+  const [curentPage, setCurentPage] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname.split('/');
+
+    if (path[2] === 'settings' || path[2] === 'profile') {
+      setCurentPage('profile');
+    } else if (path[2] === 'password') {
+      setCurentPage('password');
+    }
+  }, [location.pathname]);
+
   return (
-    <div className={styles.Settings}>
-      <h1>Settings</h1>
-      <Link to="/dashboard/profile">Profile</Link>
-      <Link to="/dashboard/password">Password</Link>
+    <main className={styles.Settings}>
+      <section className={styles.header}>
+        <h1>Settings</h1>
+      </section>
+      <nav className={styles.nav}>
+        <Link
+          className={
+            curentPage === 'profile' ? styles.linkHighlight : styles.link
+          }
+          to="/dashboard/profile"
+        >
+          Profile
+        </Link>
+        <Link
+          className={
+            curentPage === 'password' ? styles.linkHighlight : styles.link
+          }
+          to="/dashboard/password"
+        >
+          Password
+        </Link>
+      </nav>
       <Outlet />
-    </div>
+    </main>
   );
 };
 
