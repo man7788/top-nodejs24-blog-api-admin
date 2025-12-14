44b34d26a009b450d5270ab729ea1efe6ee53563
man7788 <jerman1841@gmail.com>
2025-12-15 03:34:41 +0800

    update Overview render

--- src/components/Dashboard/overview/Overview.jsx
+++ src/components/Dashboard/overview/Overview.jsx
@@ -5,26 +5,41 @@ const Overview = () => {
   const { posts, error, loading } = usePosts();
 
   if (loading) {
-    return <h1 className={styles.h1}>loading...</h1>;
+    return (
+      <main className={styles.Overview}>
+        <h1 className={styles.header}>Overview</h1>
+        <div className={styles.altMessage}>Loading...</div>
+      </main>
+    );
   }
 
   if (error?.statusCode === 401) {
-    return <h1 className={styles.h1}>401 - Unauthorized</h1>;
+    return (
+      <main className={styles.Overview}>
+        <h1 className={styles.header}>Overview</h1>
+        <div className={styles.altMessage}>401 - Unauthorized</div>
+      </main>
+    );
   }
 
   if (error) {
-    return <h1 className={styles.h1}>A network error was encountered</h1>;
+    return (
+      <main className={styles.Overview}>
+        <h1 className={styles.header}>Overview</h1>
+        <div className={styles.altMessage}>A network error was encountered</div>
+      </main>
+    );
   }
 
   return (
     <main className={styles.Overview}>
-      <section className={styles.header}>
-        <h1>Overview</h1>
-      </section>
-      <section className={styles.section}>
-        <h2 className={styles.h2}>Statistics</h2>
-        <p className={styles.p}>{posts?.length} posts</p>
-      </section>
+      <h1 className={styles.header}>Overview</h1>
+      <article className={styles.article}>
+        <header>
+          <h2 className={styles.h2}>Statistics</h2>
+        </header>
+        <div className={styles.statistics}>{posts?.length} posts</div>
+      </article>
     </main>
   );
 };
