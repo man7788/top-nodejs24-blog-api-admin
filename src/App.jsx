1bdb10ccf8ac0ac04a11d2dd9b691dfaa394ebed
man7788 <jerman1841@gmail.com>
2026-01-02 00:37:39 +0800

    update App render

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,37 @@ import { Navigate } from 'react-router';
 import useAuth from './hooks/useAuth';
 
 function App() {
-  const { user, loading } = useAuth();
+  const { user, loading, error } = useAuth();
 
-  if (loading) return <h1 className={styles.loading}>Loading...</h1>;
+  if (loading) {
+    return (
+      <main className={styles.App}>
+        <h1 className={styles.header}>Blog API</h1>
+        <div className={styles.message}>Loading...</div>
+      </main>
+    );
+  }
 
-  return (
-    <div className={styles.App}>
-      {user ? (
-        <Navigate to="/dashboard" replace={true} />
-      ) : (
-        <Navigate to="/login" replace={true} />
-      )}
-    </div>
-  );
+  if (error.statusCode === 401) {
+    return (
+      <div className={styles.App}>
+        {user ? (
+          <Navigate to="/dashboard" replace={true} />
+        ) : (
+          <Navigate to="/login" replace={true} />
+        )}
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <main className={styles.App}>
+        <h1 className={styles.header}>Blog API</h1>
+        <div className={styles.message}>A network error was encountered</div>
+      </main>
+    );
+  }
 }
 
 export default App;
