0ffe141ae7b987e7592f598568f93db767295d0d
man7788 <jerman1841@gmail.com>
2026-01-15 02:47:31 +0800

    update PrivateRoute render

--- src/components/privateRoute/PrivateRoute.jsx
+++ src/components/privateRoute/PrivateRoute.jsx
@@ -4,9 +4,34 @@ import useAuth from '../../hooks/useAuth';
 import { Navigate } from 'react-router';
 
 const PrivateRoute = () => {
-  const { user, loading } = useAuth();
+  const { user, error, loading } = useAuth();
 
-  if (loading) return <h1 className={styles.loading}>Loading...</h1>;
+  if (loading) {
+    return (
+      <main className={styles.PrivateRoute}>
+        <h1 className={styles.header}>Blog API</h1>
+        <div className={styles.message}>Loading...</div>
+      </main>
+    );
+  }
+
+  if (error?.statusCode === 401) {
+    return (
+      <main className={styles.PrivateRoute}>
+        <h1 className={styles.header}>Blog API</h1>
+        <div className={styles.message}>401 - Unauthorized</div>
+      </main>
+    );
+  }
+
+  if (error) {
+    return (
+      <main className={styles.PrivateRoute}>
+        <h1 className={styles.header}>Blog API</h1>
+        <div className={styles.message}>A network error was encountered</div>
+      </main>
+    );
+  }
 
   if (!user) {
     return <Navigate to="/login" replace={true} />;
